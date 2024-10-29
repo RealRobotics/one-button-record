91e36326b3537d1ed5e440a43a0b301fddccdbb5
Andy Blight <a.j.blight@leeds.ac.uk>
2024-10-29 20:22:09 +0000

    Subscriber setup done

--- one_button_record/one_button_record/hardware_interface.py
+++ one_button_record/one_button_record/hardware_interface.py
@@ -7,12 +7,9 @@ LEDState = Enum("LEDState", ["POWER_ON", "READY", "RECORDING", "ERROR"])
 class HardwareInterface:
     def __init__(self):
         pass
-
-    def _get_button_state(self) -> bool:
-        # Simulate the button state.
-        if input("Press Enter to simulate button press: "):
-            return True
-        return False
+        # Setup the LED and button GPIO pins.
+        # GPIO.setup(led_pin, GPIO.OUT)
+        # GPIO.setup(button_pin, GPIO.IN)
 
     def _set_power_on(self):
         print("Powering on...")
@@ -22,6 +19,12 @@ class HardwareInterface:
 
     def _set_recording(self):
         print("Recording...")
+
+    def get_button_state(self) -> bool:
+        # Simulate the button state.
+        if input("Press Enter to simulate button press: "):
+            return True
+        return False
 
     def set_led_state(self, state: LEDState):
         if state == LEDState.POWER_ON:
--- one_button_record/one_button_record/one_button_record.py
+++ one_button_record/one_button_record/one_button_record.py
@@ -1,30 +1,67 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.executors import ExternalShutdownException
 import time
-import one_button_record.hardware_interface
+import one_button_record.hardware_interface as hw_if
+
+from sensor_msgs.msg import CompressedImage
 
 
-def main():
-    print("Hi from one_button_record.")
-    # Create an instance of the HardwareInterface class
-    # with pin_id = 18 and type = "out"
-    hw = one_button_record.hardware_interface.HardwareInterface()
+class ImageSubscriberNode(Node):
+    def __init__(self):
+        super().__init__("image_subscriber")
+        self.subscription = self.create_subscription(
+            CompressedImage, "camera/compressed_image", self.listener_callback, 10
+        )
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+
+def boot_up(hw):
     # Simulate booting up the hardware.
-    hw.set_led_state(one_button_record.hardware_interface.LEDState.POWER_ON)
+    hw.set_led_state(hw_if.LEDState.POWER_ON)
     time.sleep(1)
-    hw.set_led_state(one_button_record.hardware_interface.LEDState.READY)
-    # Simulate the button press.
+    hw.set_led_state(hw_if.LEDState.READY)
+
+
+def test_hw(hw):
     recording = False
-    while True:
-        if hw._get_button_state():
+    completed = False
+    while not completed:
+        if hw.get_button_state():
             if recording:
                 recording = False
             else:
                 recording = True
             if recording:
                 hw.set_led_state(
-                    one_button_record.hardware_interface.LEDState.RECORDING
+                    hw_if.LEDState.RECORDING
                 )
             else:
-                hw.set_led_state(one_button_record.hardware_interface.LEDState.READY)
+                hw.set_led_state(hw_if.LEDState.READY)
+                completed = True
+
+
+def main(args=None):
+    print("Hi from one_button_record.")
+    hw = hw_if.HardwareInterface()
+    boot_up(hw)
+    test_hw(hw)
+
+    print("Subscribing to image topic.")
+    rclpy.init(args=args)
+    image_subscriber = ImageSubscriberNode()
+    try:
+        rclpy.spin(image_subscriber)
+    except (KeyboardInterrupt, ExternalShutdownException):
+        pass
+    image_subscriber.destroy_node()
+    # Fix me.
+    # Generates exception after Ctrl-C.
+    # Looks like it is a bug in rclpy.
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
