88fb984f7c2a4b89f286ee535f7b474a16645e29
Andy Blight <a.j.blight@leeds.ac.uk>
2024-10-29 18:59:30 +0000

    Implemented fake hardware interface

---
+++ one_button_record/one_button_record/hardware_interface.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+# Define the LED states
+LEDState = Enum("LEDState", ["POWER_ON", "READY", "RECORDING", "ERROR"])
+
+
+class HardwareInterface:
+    def __init__(self):
+        pass
+
+    def _get_button_state(self) -> bool:
+        # Simulate the button state.
+        if input("Press Enter to simulate button press: "):
+            return True
+        return False
+
+    def _set_power_on(self):
+        print("Powering on...")
+
+    def _set_ready(self):
+        print("Ready...")
+
+    def _set_recording(self):
+        print("Recording...")
+
+    def set_led_state(self, state: LEDState):
+        if state == LEDState.POWER_ON:
+            self._set_power_on()
+        elif state == LEDState.READY:
+            self._set_ready()
+        elif state == LEDState.RECORDING:
+            self._set_recording()
+        else:
+            print("ERROR")
--- one_button_record/one_button_record/one_button_record.py
+++ one_button_record/one_button_record/one_button_record.py
@@ -1,6 +1,31 @@
+import time
+import one_button_record.hardware_interface
+
+
 def main():
-    print('Hi from one_button_record.')
+    print("Hi from one_button_record.")
+    # Create an instance of the HardwareInterface class
+    # with pin_id = 18 and type = "out"
+    hw = one_button_record.hardware_interface.HardwareInterface()
+    # Simulate booting up the hardware.
+    hw.set_led_state(one_button_record.hardware_interface.LEDState.POWER_ON)
+    time.sleep(1)
+    hw.set_led_state(one_button_record.hardware_interface.LEDState.READY)
+    # Simulate the button press.
+    recording = False
+    while True:
+        if hw._get_button_state():
+            if recording:
+                recording = False
+            else:
+                recording = True
+            if recording:
+                hw.set_led_state(
+                    one_button_record.hardware_interface.LEDState.RECORDING
+                )
+            else:
+                hw.set_led_state(one_button_record.hardware_interface.LEDState.READY)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
